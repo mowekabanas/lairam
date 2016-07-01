d7604113f571e9f263d23a8ffcac877746cf5495
dudusbarros <joseeduardobarros@gmail.com>
2016-07-01 16:34:51 -0300

    Improve Layout Improve Main.css Improve Header.css Improve Brand.css Improve Logo.css Improve Nav.css Add Logo.js

---
+++ js/logo.js
@@ -0,0 +1,79 @@
+
+/* Mowe Logo 1.0 */
+
+var Logo = (function () {
+
+	/**
+	 * SVG Logo request
+	 * @param element {Element}
+	 * @param url {string}
+	 * @param fallback {object}
+	 * @constructor
+	 */
+	function Logo(element, url, fallback) {
+
+		var self = this;
+
+		this.element = element;
+		this.url = url;
+		this.fallback = fallback;
+
+		this.get();
+
+	}
+
+	/**
+	 * Append to element
+	 * @param toElement {Element}
+	 * @param before {Element}
+	 */
+	Logo.prototype.appendTo = function (toElement, before) {
+
+		if (!before)
+			toElement.appendChild(this.element);
+		else
+			toElement.insertBefore(this.element, before);
+
+	};
+
+	/**
+	 * Clone the logo and append to element
+	 * @param toElement {Element}
+	 */
+	Logo.prototype.cloneTo = function (toElement) {
+
+		toElement.appendChild(this.element.cloneNode(this.element));
+
+	};
+
+	Logo.prototype.get = function () {
+
+		var self = this;
+
+		if (this.element && this.url) {
+
+			var request = new XMLHttpRequest();
+			request.open('GET', this.url, true);
+
+			request.onreadystatechange = function() {
+
+				if (this.readyState === 4)
+					if (this.status == 200)
+						if (this.responseText) {
+							self.element.innerHTML = this.responseText;
+							if (self.fallback)
+								self.fallback();
+						}
+
+			};
+
+			request.send();
+			request = null;
+
+		}
+
+	};
+
+	return Logo;
+
+})();
